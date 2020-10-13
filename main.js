8b96f864928bfdf8e9a15b8ad5f71cce6efb3880
Justin Odey Adoga <44867406+pjadoga@users.noreply.github.com>
2020-10-13 02:21:55 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+// WHAT TO LOOK OUT FOR
+//**arrow function */
+//**promises  */
+//**generators */
+//**let and const  */
+//**classes */
+//**modules */
+//**multiline strings */
+//**template literals */
+//**defaultparameters */
+//**the spread operators */
+// destructuring assignment
+// enhanced object lliterals
+// the for..of loop
+// map and set
+
+// AARROW FUNCTON
+const greet = () => {
+  var num = 5;
+  return num++;
+}
+console.log(greet());
+
+// weather api call
+let ApiCall = 'api.openweathermap.org/data/2.5/weather?q=London&appid=f2efb2dc810b16d1adf76aa6251b027c';
+console.log(ApiCall);
